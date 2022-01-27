435e5644975836d7afbb38139a8112662b41a931
Philip Turner <philipturner.AR@gmail.com>
2022-01-26 21:01:56 -0500

    Update file.swift

--- file.swift
+++ file.swift
@@ -1,1 +1,17 @@
 import Swift
+
+public protocol DifferentiableCollection: Collection {}
+
+public struct DifferentiableCollectionView<Base: Collection> {
+  // Will add conditional conformance to different collectionsby restricting
+  // `Base`
+  var _base: Base
+}
+
+extension DifferentiableCollection {
+  typealias DifferentiableView = DifferentiableCollectionView<Self>
+}
+
+extension DifferentiableCollectionView {
+  
+}
