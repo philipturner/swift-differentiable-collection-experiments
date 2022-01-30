51c527d3be110bf250d1987f3269de83a9ad2e74
Philip Turner <philipturner.AR@gmail.com>
2022-01-29 20:01:44 -0500

    Update release-toolchain-workaround.swift

--- release-toolchain-workaround.swift
+++ release-toolchain-workaround.swift
@@ -1,4 +1,4 @@
-//===--- ArrayDifferentiation.swift ---------------------------*- swift -*-===//
+//===--- CollectionDifferentiation.swift ---------------------------*- swift -*-===//
 //
 // This source file is part of the Swift.org open source project
 //
